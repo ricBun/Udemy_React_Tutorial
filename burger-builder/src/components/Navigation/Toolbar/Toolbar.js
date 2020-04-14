eea779876682a2c2d7e36272393cf45f2f01da18
Richard <richardbun10@gmail.com>
2020-04-14 13:03:46 -0400

    save before folder structure change

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -4,12 +4,15 @@ import classes from './Toolbar.module.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo></Logo>
-    <nav>
+    <DrawerToggle clicked={props.drawerToggleClicked}></DrawerToggle>
+    <div className={classes.Logo}>
+      <Logo></Logo>  
+    </div>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems></NavigationItems>
     </nav>
   </header>
