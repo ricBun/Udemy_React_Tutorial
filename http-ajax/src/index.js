a6c9f8d711fa39b57254dda794062e551ee37d3e
Richard <richardbun10@gmail.com>
2020-04-15 15:32:17 -0400

    added source code for axios-http practice

---
+++ http-ajax/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// global base url
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// register interceptor
+axios.interceptors.request.use(request => {
+  console.log(request);
+  // can edit the request config before returning
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
